c988b38cd34c82eaa8bdd23dcd1a6001780dd0d5
tamawyhub <37642949+tamawyhub@users.noreply.github.com>
2023-07-16 06:26:11 +0200

    AVLTree: add r-value copy constructor and assignment operator (MINOR)

--- AVLTree.hpp
+++ AVLTree.hpp
@@ -6,9 +6,11 @@ template <typename T>
 class AVLTree : public BSTree<T> {
 	public:
 	AVLTree(): BSTree<T>(){}
-	AVLTree (const T &v): BSTree<T>(v){} //empty constructor
-	AVLTree (const BSTree<T> &bst): BSTree<T>(bst) {}; //copy constructor
-	AVLTree<T>& operator = (const BSTree<T> &bst) {BSTree<T>::operator=(bst);}
+	AVLTree (const T &v): BSTree<T>(v){}
+	AVLTree (const BSTree<T> &bst): BSTree<T>(bst) {}
+	AVLTree (const BSTree<T> &&bst): BSTree<T>(bst) {}
+	AVLTree<T>& operator = (const BSTree<T> &bst) {BSTree<T>::operator=(bst); return *this;}
+	AVLTree<T>& operator = (const BSTree<T> &&bst) {BSTree<T>::operator=(bst); return *this;}
 	void insert(const T &value);
 	void remove(const T &value);
 	private:
